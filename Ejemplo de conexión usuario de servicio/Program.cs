6f8077d93a847ff29166ac3409eb721991dccdf8
Eze Baravalle <ezebaravalle@gmail.com>
2022-08-26 18:34:18 -0300

    Ejemplo de conexión con usuario de servicio

---
+++ Ejemplo de conexión usuario de servicio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Crm.Sdk.Messages;
+using System.Net;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Tooling.Connector;
+
+// Ejemplo realizado por Ezequiel Matias Baravalle, 
+//visitá mi blog: https://ezequielbaravalle.blogspot.com/
+
+namespace Ejemplo_de_conexión_usuario_de_servicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            //Se coloca la URL del entorno D365 al cual deseamos conectarnos
+            string url_entorno = "https://entornoD365.crm2.dynamics.com";
+            // Es el ID de la aplicación que se registró en Azure y se asoció al entorno D365
+            string clientId = "dfd45868-71f3-4259-b231-g773308d2ad2"; // Este valor proporcionado es un ejemplo
+            // Es el secreto generado para la aplicación en azure
+            string clientSecret = "o4rrt~K9ggAAgyx-B5TN3q4cJF3br1kMm.LWWbae"; // Este valor proporcionado es un ejemplo
+            //Con los datos anteriores, ya se puede armar la cadena de conexión hacia D365
+            string conectionString = $@"Url = {url_entorno};AuthType = ClientSecret;ClientId = {clientId};ClientSecret = {clientSecret};";
+
+            using (var svc = new CrmServiceClient(conectionString))
+            {
+
+                if (svc.IsReady)
+                {
+                    Console.WriteLine("¡Felicitaciones, te has conectado correctamente a tu entorno de D365!");
+
+                    Console.WriteLine("Obteniendo tu user id...");
+
+                    WhoAmIRequest request = new WhoAmIRequest();
+
+                    WhoAmIResponse response = (WhoAmIResponse)svc.Execute(request);
+
+                    Console.WriteLine("Tu UserId es {0}", response.UserId);
+
+                    Console.WriteLine("Para salir, presioná cualquier tecla.");
+                }
+                else Console.WriteLine("Error, no fue posible conectarnos con tu entorno de D365.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
