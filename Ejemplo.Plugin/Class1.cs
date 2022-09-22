3514a9078588a159ce324349f029503a9e95dec8
Ezequiel Baravalle <69125001+baraveze@users.noreply.github.com>
2022-09-22 14:43:28 -0300

    Update Class1.cs

--- Ejemplo.Plugin/Class1.cs
+++ Ejemplo.Plugin/Class1.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
 
 namespace Ejemplo.Plugin
 {
-    public class Class1
+    public class Class1 : IPlugin
     {
         // Acá va la lógica del código
+          public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+            Entity Target = (Entity)context.InputParameters["Target"];
+            
+            try{
+            
+            }catch(Exception e)
+            {
+              throw new InvalidPluginExecutionException("Mensaje de error"+e.message);
+            }
+         }
     }
 }
